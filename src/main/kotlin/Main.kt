93a9f7f32c3d30c941442058250a4e42b784d108
Elisee-dev <esabidani@gmail.com>
2022-07-29 13:49:07 +0200

    OK pour les fonction simple

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,19 +1,20 @@
 fun main() {
-
-    var num = 0
-    println("Resultat avec do")
-    while (num > 0)
-    {
-        println("num = $num")
-        num--
-
-    }
-
-    println("Resultat avec do..while")
-    do
-    {
-        println("num = $num")
-        num--
-
-    }while (num > 0)
+    aficheBienvenu("Mr Sabidani")
+    val resultSomme = somme(20, 12)
+    println("le resultat de la somme est $resultSomme")
+    println("le resultat de la multiplication est ${multiplication(10, 20)}")
 }
+
+fun  aficheBienvenu(nom: String)
+{
+    println("Bienvenu $nom")
+}
+
+fun somme(num1: Int, num2: Int): Int
+{
+    val somme = num1 + num2
+
+    return somme
+}
+
+fun multiplication(num1: Int, num2: Int) = num1 * num2
