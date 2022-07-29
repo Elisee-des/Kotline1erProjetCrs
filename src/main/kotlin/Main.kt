2a598bec4011bd41b59abbeeaea93830c6f0b9ab
Elisee-dev <esabidani@gmail.com>
2022-07-29 10:41:11 +0200

    Ok pour les Types

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,13 @@
 fun main(args: Array<String>) {
-    println("Hello")
-    print("word")
+    val a: Float = 100F
+    val e: Double = 129.2
+
+
+    val num1: Int = 100
+    val num2: Double = 199.2
+    val  stringg: String = "je test"
+    val myChar: Char = 'G'
+    val num = myChar.code
+    println("Numero1 = "+ num)
+
 }
