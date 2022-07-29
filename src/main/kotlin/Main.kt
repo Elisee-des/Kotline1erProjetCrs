90a8970f105f885aff245f4187cfc3340d3d1ff9
Elisee-dev <esabidani@gmail.com>
2022-07-29 11:21:48 +0200

    Ok pour les string et comparaison de string

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,18 @@
 fun main() {
-    var a = 10
-    a+=2
-    println("a ="+a)
-    a-=4
-    println("a ="+a)
-    a/=2
-    println("a ="+a)
-    a%=2
-    println("a ="+a)
+    val nom1 = "SAbidani"
+    val nom2 = "Bonzi"
+    val phrase = """Kotline est cool"""
+
+    println("le nom est $nom1")
+    println("la phrase est: $phrase")
+
+    val lengh = phrase.length
+    println("la phrase contient $lengh caracteres")
+    println("la nom contient ${nom1.length} caracteres")
+
+    println("nom1 == nom2 ? ${nom1 == nom2}")
+    println("nom1 == nom2 ? ${nom1.get(0)}")
+
+    println("Kotlin fait partir de $phrase? ${phrase.contains("kotlin", true)}")
 
 }
